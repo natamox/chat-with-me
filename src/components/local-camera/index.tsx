c89a08d9e020e1e23e5b5b8bd5c077bcbdf4a2aa
natamox <natamoxc@gmail.com>
2022-12-23 00:27:53 +0800

    obs

--- src/components/local-camera/index.tsx
+++ src/components/local-camera/index.tsx
@@ -1,10 +1,65 @@
-import React from 'react';
+import OBSWebSocket from 'obs-websocket-js';
+import React, { useEffect } from 'react';
+import Webcam from 'react-webcam';
+
+const myVideo = document.getElementById('local') as HTMLVideoElement;
+const obs = new OBSWebSocket();
+try {
+  const { obsWebSocketVersion, negotiatedRpcVersion } = await obs.connect('ws://localhost:4455', '[::1]:51906', {
+    rpcVersion: 1,
+  });
+  console.log(`Connected to server ${obsWebSocketVersion} (using RPC ${negotiatedRpcVersion})`);
+} catch (error) {
+  console.error('Failed to connect', error.code, error.message);
+}
+
+const getMedia = () => {
+  // 获取支持的音视频设备
+  if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices()) {
+    console.log('不支持');
+  } else {
+    navigator.mediaDevices
+      .enumerateDevices()
+      .then((deviceInfos) => {
+        deviceInfos.forEach((info) => {
+          console.log(`${info.kind}：label=${info.label}：id=${info.deviceId}：group = ${info.groupId}`);
+        });
+      })
+      .catch((err) => {
+        console.log(`${err.name}：${err.message}`);
+      });
+  }
+};
 
 function LocalCamera() {
+  const start = () => {
+    // navigator.mediaDevices
+    //   .getUserMedia({ video: true, audio: true })
+    //   .then((stream) => {
+    //     console.log('stream', stream);
+    //     myVideo.srcObject = stream;
+    //   })
+    //   .catch(console.error);
+    obs.on('ConnectionOpened', () => {
+      obs.call('GetSceneList').then((data) => {
+        console.log('data', data);
+      });
+    });
+  };
+
+  useEffect(() => {
+    getMedia();
+  }, []);
+
   return (
-    <div>
-      LocalCamera <div>2</div>
-    </div>
+    <>
+      <div>
+        <button type="button" onClick={start}>
+          init
+        </button>
+      </div>
+      <video id="local" autoPlay playsInline muted />
+    </>
   );
 }
 
--- src/pages/one-to-one-chat/index.tsx
+++ src/pages/one-to-one-chat/index.tsx
@@ -1,12 +1,14 @@
 import { Box, HStack } from '@chakra-ui/react';
+import LocalCamera from '@components/local-camera';
 import PageContainer from '@components/page-container';
+import Webcam from 'react-webcam';
 
 function OneToOneChat() {
   return (
     <PageContainer>
       <HStack>
         <Box w="50%" h="65vh" background="telegram.100" borderWidth="1px" borderRadius="base">
-          2
+          2<LocalCamera />
         </Box>
         <Box w="50%" h="65vh" background="chakra-border-color" borderWidth="1px" borderRadius="base">
           3
