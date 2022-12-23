6986fc07d5dd489b53e7dfd5faab19cb41f3620f
natamox <natamoxc@gmail.com>
2022-12-23 17:48:43 +0800

    feat: chrome 浏览器获取不到虚拟摄像头，只能用 firefox 了

--- src/components/local-camera/index.tsx
+++ src/components/local-camera/index.tsx
@@ -1,66 +1,15 @@
-import OBSWebSocket from 'obs-websocket-js';
-import React, { useEffect } from 'react';
 import Webcam from 'react-webcam';
 
-const myVideo = document.getElementById('local') as HTMLVideoElement;
-const obs = new OBSWebSocket();
-try {
-  const { obsWebSocketVersion, negotiatedRpcVersion } = await obs.connect('ws://localhost:4455', '[::1]:51906', {
-    rpcVersion: 1,
-  });
-  console.log(`Connected to server ${obsWebSocketVersion} (using RPC ${negotiatedRpcVersion})`);
-} catch (error) {
-  console.log(error);
-}
-
-const getMedia = () => {
-  // 获取支持的音视频设备
-  if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices()) {
-    console.log('不支持');
-  } else {
-    navigator.mediaDevices
-      .enumerateDevices()
-      .then((deviceInfos) => {
-        deviceInfos.forEach((info) => {
-          console.log(`${info.kind}：label=${info.label}：id=${info.deviceId}：group = ${info.groupId}`);
-        });
-      })
-      .catch((err) => {
-        console.log(`${err.name}：${err.message}`);
-      });
-  }
+const videoConstraints = {
+  width: { min: 480 },
+  height: { min: 720 },
+  aspectRatio: 0.6666666667,
 };
-
 function LocalCamera() {
-  const start = () => {
-    // navigator.mediaDevices
-    //   .getUserMedia({ video: true, audio: true })
-    //   .then((stream) => {
-    //     console.log('stream', stream);
-    //     myVideo.srcObject = stream;
-    //   })
-    //   .catch(console.error);
-    obs.on('ConnectionOpened', () => {
-      obs.call('GetSceneList').then((data) => {
-        console.log('data', data);
-      });
-    });
+  const onCallBack = (stream: MediaStream) => {
+    console.log('stream', stream.getTracks());
   };
-
-  useEffect(() => {
-    getMedia();
-  }, []);
-
-  return (
-    <>
-      <div>
-        <button type="button" onClick={start}>
-          init
-        </button>
-      </div>
-      <video id="local" autoPlay playsInline muted />
-    </>
-  );
+  return <Webcam onUserMedia={onCallBack} />;
 }
 
 export default LocalCamera;
--- src/pages/one-to-one-chat/index.tsx
+++ src/pages/one-to-one-chat/index.tsx
@@ -1,18 +1,17 @@
-import { Box, HStack } from '@chakra-ui/react';
+import { Center, HStack } from '@chakra-ui/react';
 import LocalCamera from '@components/local-camera';
 import PageContainer from '@components/page-container';
-import Webcam from 'react-webcam';
 
 function OneToOneChat() {
   return (
     <PageContainer>
       <HStack>
-        <Box w="50%" h="65vh" background="telegram.100" borderWidth="1px" borderRadius="base">
-          2<LocalCamera />
-        </Box>
-        <Box w="50%" h="65vh" background="chakra-border-color" borderWidth="1px" borderRadius="base">
-          3
-        </Box>
+        <Center minH="400px" w="50%" p={4} background="telegram.100" borderWidth="1px" borderRadius="base">
+          <LocalCamera />
+        </Center>
+        <Center minH="400px" w="50%" p={4} background="chakra-border-color" borderWidth="1px" borderRadius="base">
+          <LocalCamera />
+        </Center>
       </HStack>
     </PageContainer>
   );
